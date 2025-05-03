e1f7ad0d2bc3112ecc0d116f63f846605d9f896f
elmirmammadli1995 <149464393+elmirmammadli1995@users.noreply.github.com>
2025-05-03 12:47:13 +0000

    SettingsCommited

--- newsapi/settings.py
+++ newsapi/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
 ] 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleeware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -42,6 +44,12 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://elmirmammadli1995.github.io",
+]
+
 
 ROOT_URLCONF = "newsapi.urls"
 
