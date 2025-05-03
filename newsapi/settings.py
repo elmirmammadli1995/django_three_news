a6a52efd4bface6aafe10b21ba4ef67934d06d0e
elmirmammadli1995 <149464393+elmirmammadli1995@users.noreply.github.com>
2025-05-03 12:53:44 +0000

    commited

--- newsapi/settings.py
+++ newsapi/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleeware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
